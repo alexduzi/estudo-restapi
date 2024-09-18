a2e3bd7df20591a141273a9ad1525384b0b5dcff
Alex Duzi <alex.henrique.duzi@accenture.com>
2024-09-18 20:00:37 -0300

    commit inicial

---
+++ src/main/java/home/restapi/estudo_restapi/EstudoRestapiApplication.java
@@ -0,0 +1,13 @@
+package home.restapi.estudo_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/restapi/estudo_restapi/EstudoRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package home.restapi.estudo_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
